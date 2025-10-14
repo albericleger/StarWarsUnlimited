7b810da81fd3cbf4fad6a925e8c8aecda7594bb4
albericleger <albericleger@gmail.com>
2025-10-14 18:51:45 +0200

    Initial Commit

---
+++ Star Wars Unlimited/Star_Wars_UnlimitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_UnlimitedApp.swift
+//  Star Wars Unlimited
+//
+//  Created by Albéric Léger on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_UnlimitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
